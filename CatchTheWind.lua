95c5ce70853a1c06a7c1c401cda6e69863e4021c
lanrutcon <lanrutcon.git@gmail.com>
2016-08-05 19:37:18 +0100

    Multiple fixes, QuestRewardsPanel overhaul, LuaDoc
    * Fixed QuestRewardPanel whenever the player doesn't click.
    * QuestRewardPanel modified (it now appears in the middle).
    * Cleaned-up some code.
    * .toc file updated.
    * LuaDoc added.

--- CatchTheWind.lua
+++ CatchTheWind.lua
@@ -1,6 +1,8 @@
 local Addon = CreateFrame("FRAME");
 
+--Main UI Frame
 local letterBox;
+--Variable Quest Text Speed
 local factorTextSpeed = 1;
 
 --TODO
@@ -8,16 +10,17 @@ local factorTextSpeed = 1;
 --
 -- Cinematic Quests AddOn
 --x * GetTitleText + GetProgressText + GetObjectiveText + GetRewardText
--- * Set a close-plan to player when deciding "ACCEPT/DECLINE" and choosing rewards.
---x * Show rewards only after the last line. ("QUEST_COMPLETE")
---x * Add SlashCommand to modify text speed
+-- * Set a close-plan to player when deciding "ACCEPT/DECLINE" and choosing rewards. (check for libCameras)
 --x * Mouse-Click shows all the message (if it's still animating). Another click passes to the next line.
 -- * SpaceBar shows all the message. This may bring problems because we have to enable keyboard.
+--
+-- * Start to think in moving GUI (frames and buttons) to a XML file
+-- * It's about time to add documentation
 --
 -- QUEST PROGRESS > QUEST COMPLETE > QUEST FINISHED (it means you stop interacting with a NPC about a quest) > QUEST LOG UPDATED
 
 -- BUGS:
---x * When interacting with merchants: GOSSIP SHOW > CLOSE > MERCHANT SHOW (CHECK IN WoD)
+-- * Some players have problems in "Accept/Decline" buttons, i.e. they can't click on them. (Guess: Addons are in-front -> set a higher frame level)
 --x * WoD: When the NPC Questgiver only has 1 quest, it automatically goes to "QUEST_DETAIL"/"QUEST_PROGRESS". It doesn't pass through "GOSSIP" events.
 
 --x = Done/Fixed
@@ -26,7 +29,15 @@ local factorTextSpeed = 1;
 --UTILS
 --------------------
 
---Used in SlashCommands
+
+-------------------------------------
+--
+-- Prints the given string at yellow.
+-- If it contains "CatchTheWind" then it colors it in a gray gradient.
+-- Used in SlashCommands.
+-- @param #string msg : the message that will be printed
+--
+-------------------------------------
 local function printMessage(msg)
 	if(string.find(msg, "CatchTheWind")) then
 		local msg = string.sub(msg, string.find(msg, "CatchTheWind")+12, -1);
@@ -37,9 +48,15 @@ local function printMessage(msg)
 end
 
 
-
+--Timer Frame
 local timer = CreateFrame("FRAME");
-
+-------------------------------------
+--
+-- Creates a timer. After the specified time, it will execute the given function.
+-- @param #number after : the message that will be printed
+-- @param #function func : the message that will be printed
+--
+-------------------------------------
 local function createTimer(after, func)
 	local total = 0;
 	timer:SetScript("OnUpdate", function(self, elapsed)
@@ -52,30 +69,54 @@ local function createTimer(after, func)
 end
 
 
+-------------------------------------
+--
+-- Cancels the timer.
+--
+-------------------------------------
 local function cancelTimer()
 	timer:SetScript("OnUpdate", nil);
 end
 
 
---split a text (multiple lines) and store all lines in a table - each entry is a line
+-------------------------------------
+--
+-- Splits the given text (string with \n (aka "Enter")).
+-- It returns a table, in each entry is paragraph from the text.
+-- @param #string text : the string that will be splitted
+-- @return #table tableLines: the table with the lines from the text
+--
+-------------------------------------
 local function splitText(text)
-	local lines, nextLine = {};
+	local tableLines, nextLine = {};
 	while string.find(text,"\n") do
 		nextLine = string.sub(text,0, string.find(text, "\n"));
 		if not (strtrim(nextLine) == "") then
-			table.insert(lines, nextLine);
+			table.insert(tableLines, nextLine);
 		end
 		text = string.sub(text, string.find(text, "\n")+1, -1);	
 	end
 	if not (strtrim(text) == "") then
-		table.insert(lines, string.sub(text, 0, string.find(text, "\n")));
+		table.insert(tableLines, string.sub(text, 0, string.find(text, "\n")));
 	end
-	return lines;
+	return tableLines;
 end
 
 
-
---creates a button frame
+-------------------------------------
+--
+-- Creates a button with the given values.
+-- This button is a "FRAME" with a "FontString".
+-- Used to create the "Accept"/"Decline" buttons.
+-- @param #string name : the button's name
+-- @param #frame parent : the frame that will be attached
+-- @param #string point : the anchor's point
+-- @param #number xOfs : the variation in the X axis from the anchor
+-- @param #number yOfs : the variation in the Y axis from the anchor
+-- @param #function onClickFunc : the function that will be executed whenever this button is pressed
+-- @return #frame buttonFrame: the button itself
+--
+-------------------------------------
 local function createButton(name, parent, text, point, xOfs, yOfs, onClickFunc)
 	local buttonFrame = CreateFrame("FRAME", name, parent);
 	buttonFrame:SetSize(200,50);
@@ -93,9 +134,12 @@ local function createButton(name, parent, text, point, xOfs, yOfs, onClickFunc)
 		self.fontString:SetTextColor(1, 1, 1, 1);
 	end);
 	buttonFrame:SetScript("OnLeave", function(self)
-		self.fontString:SetTextColor(0.3, 0.3, 0.3, 1);
+		self.fontString:SetTextColor(0.45, 0.45, 0.45, 1);
 	end);
 	
+	buttonFrame:SetScript("OnShow", function(self)
+		UIFrameFadeIn(self, 0.5, 0, 1);
+	end);
 	
 	buttonFrame:Hide();
 	
@@ -103,34 +147,54 @@ local function createButton(name, parent, text, point, xOfs, yOfs, onClickFunc)
 end
 
 
-
---fontString animator
-local animateFrame, isAnimating = CreateFrame("FRAME");
+--Frame, Boolean
+local animationFrame, isAnimating = CreateFrame("FRAME");
+-------------------------------------
+--
+-- Animates the given fontString.
+-- Uses "SetAlphaGradient" in order to fade in the text.
+-- @param #fontString fontString : the fontString that will be animated
+--
+-------------------------------------
 local function animateText(fontString)
 	local total, numChars = 0, 0;
 	fontString:SetAlphaGradient(0,20);
 	isAnimating = true;
-	animateFrame:SetScript("OnUpdate", function(self, elapsed)
+	animationFrame:SetScript("OnUpdate", function(self, elapsed)
 		numChars = numChars + 0.25*factorTextSpeed;
 		fontString:SetAlphaGradient(numChars,20);
-		if(numChars >= string.len(fontString:GetText())) then
+		if(numChars >= string.len(fontString:GetText() or "")) then
 			isAnimating = false;
 			self:SetScript("OnUpdate", nil);
 		end
 	end);
 end
 
---returns true if the text is still animating (i.e. fading in)
+
+-------------------------------------
+--
+-- Checks if the text is being animated.
+-- If the text is "full-shown" then it will return false.
+-- @return #boolean isAnimating : true, if the animation is ON
+--
+-------------------------------------
 local function isTextAnimating()
 	return isAnimating;
 end
 
 
+--Storing SetView function (Blizzard's function)
 local blizz_SetView = SetView;
---SetView modified for addOn needs
---It will check first, if view that we are setting is the same that we already have.
---This avoid the problem of calling the same view, which instantly sets the view (no smooth movement)
+--Current view
 local currentView = -1;
+-------------------------------------
+--
+-- Sets a view.
+-- It checks first, if the view that will be set is the current one.
+-- This prevents instant changes (i.e. no smooth movement).
+-- @param #number view: the desired view [1-5]
+--
+-------------------------------------
 local function SetView(view)
 	if not (view == currentView) then
 		currentView = view;
@@ -145,9 +209,16 @@ end
 
 
 
---UIFrameFadeOut doesn't hide the frame in the end.
---Also, UIFrameFade calls :Show() which taints whenever the player is in-combat. (UIParent:Show())
+--Frame Fader
 local frameFader = CreateFrame("FRAME");
+
+-------------------------------------
+--
+-- Hides the letterbox and its containers.
+-- It fades out the frame and its containers.
+-- Also, it fades in the UIParent.
+--
+-------------------------------------
 local function hideLetterBox()
 	--UIFrameFadeIn(UIParent, 0.25, 0, 1);	It's not advised to use UIFrameFade on "UIParent" because it taints the code
 	local alpha = UIParent:GetAlpha();
@@ -174,6 +245,12 @@ local function hideLetterBox()
 end
 
 
+-------------------------------------
+--
+-- Shows the letterbox and its containers.
+-- It also sets the UIParent's alpha to 0.
+--
+-------------------------------------
 local function showLetterBox()
 	UIParent:SetAlpha(0);
 	MinimapCluster:Hide(); --Minimap icons aren't affected with "SetAlpha"
@@ -181,8 +258,12 @@ local function showLetterBox()
 end
 
 
---starts the interaction (text/speech) after a quest event
---TODO: GET A BETTER NAME
+-------------------------------------
+--
+-- Inits the interaction with the NPC after choosing a quest.
+-- Used when QuestEvents triggers.
+--
+-------------------------------------
 local function startInteraction()
 	letterBox.acceptButton:Hide();
 	letterBox.declineButton:Hide();
@@ -196,11 +277,39 @@ local function startInteraction()
 end
 
 
+--In the next update, this will be moved to XML
+-------------------------------------
+--
+-- Creates Quest Reward Panel and all its containers.
+-- Used in "setUpLetterBox" function.
+--
+-------------------------------------
 local function createQuestRewardPanel()
 
 	--quest reward panel
 	letterBox.rewardPanel = CreateFrame("FRAME", "CTWrewardPanel", letterBox);
-	letterBox.rewardPanel:SetPoint("LEFT");
+	local rewardPanel = letterBox.rewardPanel;	--referencing to smaller name
+	rewardPanel:SetSize(600,100);
+	rewardPanel:SetPoint("TOP", letterBox.bottomPanel, 0, 100);
+	
+	--creating background textures (some a gradient that fades in and out)
+	rewardPanel.textureCenter = rewardPanel:CreateTexture(nil, "BACKGROUND");
+	rewardPanel.textureCenter:SetSize(200,100);
+	rewardPanel.textureCenter:SetColorTexture(0,0,0);
+	rewardPanel.textureCenter:SetAlpha(0.9);
+	rewardPanel.textureCenter:SetPoint("CENTER");
+	
+	rewardPanel.textureLeft = rewardPanel:CreateTexture(nil, "BACKGROUND");
+	rewardPanel.textureLeft:SetSize(200,100);
+	rewardPanel.textureLeft:SetColorTexture(0,0,0);
+	rewardPanel.textureLeft:SetGradientAlpha("HORIZONTAL", 0, 0, 0, 0, 0, 0, 0, 0.9);
+	rewardPanel.textureLeft:SetPoint("LEFT");
+	
+	rewardPanel.textureRight = rewardPanel:CreateTexture(nil, "BACKGROUND");
+	rewardPanel.textureRight:SetSize(200,100);
+	rewardPanel.textureRight:SetColorTexture(0,0,0);
+	rewardPanel.textureRight:SetGradientAlpha("HORIZONTAL", 0, 0, 0, 0.9, 0, 0, 0, 0);
+	rewardPanel.textureRight:SetPoint("RIGHT");
 	
 	
 	--quest reward panel title
@@ -208,7 +317,7 @@ local function createQuestRewardPanel()
 	letterBox.rewardPanel.title:SetFont("Fonts\\FRIZQT__.TTF", 18, "OUTLINE");
 	letterBox.rewardPanel.title:SetTextColor(1, 1, 1, 1);
 	letterBox.rewardPanel.title:SetText("Choose your reward");
-	letterBox.rewardPanel.title:SetPoint("TOP", 0, -18);
+	letterBox.rewardPanel.title:SetPoint("TOP", 0, -12);
 	
 	--quest reward items buttons(blizz has 10 buttons)
 	--refactor > extract code
@@ -235,24 +344,21 @@ local function createQuestRewardPanel()
 			insets = { left = 6, right = 6, top = 6, bottom = 6 }}
 		);
 		
-		btn:SetPoint("TOP", 0, (-i)*56);
+		btn:SetPoint("CENTER", (-i)*56, -12);
 		
 		btn:SetScript("OnEnter", function(self)
 			GameTooltip:SetParent(WorldFrame);
 			GameTooltip:SetFrameStrata("TOOLTIP");
-			ShoppingTooltip1:SetParent(WorldFrame);
-			ShoppingTooltip2:SetParent(WorldFrame);
-			ShoppingTooltip3:SetParent(WorldFrame);
+			for i=1,3 do
+				_G["ShoppingTooltip"..i]:SetParent(WorldFrame);
+				_G["ShoppingTooltip"..i]:SetFrameStrata("TOOLTIP");
+			end
 			
 			GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
 			GameTooltip:SetQuestItem(self.type, self:GetID());
 			GameTooltip_ShowCompareItem(GameTooltip);
-
-			
-			--TODO
-			--clean this tooltip code a little bit
-			
 		end);
+		
 		btn:SetScript("OnLeave", function()
 			GameTooltip:SetParent(UIParent);
 			GameTooltip:Hide();
@@ -277,10 +383,15 @@ local function createQuestRewardPanel()
 end
 
 
-
+-------------------------------------
+--
+-- Sets up all the frames for letterbox.
+--
+-------------------------------------
 local function setUpLetterBox()
 	local screenWidth = GetScreenWidth()*UIParent:GetEffectiveScale();
 	local screenHeight = GetScreenHeight()*UIParent:GetEffectiveScale();
+	
 	letterBox = CreateFrame("FRAME", "CatchTheWind", WorldFrame);
 	letterBox:SetAllPoints();
 	
@@ -326,13 +437,18 @@ local function setUpLetterBox()
 			if(self.acceptButton.fontString:GetText() == "Continue") then
 				if(IsQuestCompletable()) then
 					self.acceptButton:Show();
-					self.acceptButton:SetScript("OnMouseUp", QuestProgressCompleteButton_OnClick);
 				else
 					self.acceptButton:Hide();
 				end
 			else
 				self.acceptButton:Show();
 			end
+			if(self.acceptButton.fontString:GetText() == "Thank you" and 
+			self.textIndex == #self.text and GetNumQuestChoices() > 0 and
+			not self.rewardPanel:IsShown()) then
+				UIFrameFadeIn(self.rewardPanel, 0.5, 0, 1);
+			end
+			
 			self.declineButton:Show();
 			return;
 		end
@@ -340,16 +456,13 @@ local function setUpLetterBox()
 		if(isTextAnimating()) then
 			self.questText:SetAlphaGradient(string.len(self.questText:GetText()),1);
 			isAnimating = false;
-			animateFrame:SetScript("OnUpdate", nil);
+			animationFrame:SetScript("OnUpdate", nil);
 		else
 			self.textIndex = self.textIndex + 1;
 			self.questText:SetText(self.text[self.textIndex]);
 			animateText(self.questText);
 		end
-		--fades in Quest Reward Panel if it's the last line that is being displayed
-		if(self.acceptButton.fontString:GetText() == "Thank you" and self.textIndex == #self.text) then
-			UIFrameFadeIn(self.rewardPanel, 0.5, 0, 1);
-		end
+		
 	end);
 	
 	
@@ -360,8 +473,12 @@ local function setUpLetterBox()
 end
 
 
-
-
+-------------------------------------
+--
+-- Load SavedVariables.
+-- If there is no SVs, then it creates them.
+--
+-------------------------------------
 local function loadSavedVariables()
 	if(not CatchTheWindSV) then
 		CatchTheWindSV = {};
@@ -379,6 +496,8 @@ end
 
 ------------
 --ADDON SCRIPTS
+--
+--Some funcitons that will be used later for events.
 
 local function onPlayerLogin()
 	SaveView(5);
@@ -460,6 +579,7 @@ local function onQuestComplete()
 			SetItemButtonTexture(btn, texture);
 			_G[btn:GetName().."IconTexture"]:SetVertexColor(0.5,0.5,0.5,1);
 
+			btn:SetPoint("CENTER", (i-1)*64-(GetNumQuestChoices()/2*64)+32, -12)
 			btn:SetID(i);
 			btn:Show();
 		end
@@ -469,10 +589,6 @@ local function onQuestComplete()
 			_G["CTWrewardPanelItem"..i]:Hide();
 		end
 		
-		--set quest reward panel in the correct position (i.e. centered)
-		letterBox.rewardPanel:SetSize(200, (GetNumQuestChoices())*56);
-		letterBox.rewardPanel:SetPoint("LEFT", 0, 18);
-		
 		--letterBox.rewardPanel:Show();
 	else
 		letterBox.rewardPanel:Hide();
@@ -480,8 +596,7 @@ local function onQuestComplete()
 end
 
 
-
-
+--Table with the scripts
 Addon.scripts = {
 	["PLAYER_LOGIN"] = onPlayerLogin,
 	["GOSSIP_SHOW"] = onGossipShow,
@@ -491,12 +606,21 @@ Addon.scripts = {
 };
 
 
+--/ADDON SCRIPTS
+------------
 
---SlashCommands
---/ctw textSpeed
 
+--Slash Commands available to players
 SLASH_CatchTheWind1, SLASH_CatchTheWind2 = "/catchthewind", "/ctw";
 
+-------------------------------------
+--
+-- SlashCommand function.
+-- Commands implemented:
+-- 						- textSpeed
+-- @param #string cmd : the command that the player inputs
+--
+-------------------------------------
 local function SlashCmd(cmd)
     if (cmd:match"textSpeed") then
         local factor = tonumber(strtrim(cmd:sub(cmd:find("textSpeed")+9, -1)));
@@ -512,7 +636,15 @@ end
 SlashCmdList["CatchTheWind"] = SlashCmd;
 
 
+--Boolean (checks if the camera is moving)
 local moving = false;
+-------------------------------------
+--
+-- Addon SetScript OnEvent
+-- Starts up the addOn.
+-- Handled events can be checked right after this function.
+--
+-------------------------------------
 Addon:SetScript("OnEvent", function(self, event)
 	if(Addon.scripts[event]) then
 		Addon.scripts[event]();
